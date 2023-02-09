73bc1f28203d00cbf5c06b60a0ad56e72596c65c
AL SABAB <sabab015@gmail.com>
2023-02-09 12:23:12 +0600

    es6 basics

---
+++ template.js
@@ -0,0 +1,13 @@
+const FirstName = "Justin";
+const LastName = "Timberlake";
+
+const FullName = FirstName + ' ' + LastName;
+const FullName2 = ``
+
+console.log(FullName);
+
+const multiLine = `Here for work
+Arrived Early`;
+
+console.log(multiLine);
+
