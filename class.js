a5f5e060ad633b900a57428e7765fd826289ba14
AL SABAB <sabab015@gmail.com>
2023-02-11 11:11:27 +0600

    class

--- class.js
+++ class.js
@@ -2,9 +2,11 @@ class Student {
     constructor(sId, sName){
         this.id = sId;
         this.name = sName;
+        this.school = "Motijheel";
     }
 }
 
-const student1 = new Student();
-const student2 = new Student();
+const student1 = new Student(12, "s1");
+const student2 = new Student(17, "s2");
 console.log(student1,student2);
+
